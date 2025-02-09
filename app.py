f0964085be11de7358418c64b91dd194c70f33b9
adam <135308358+asepridwanss@users.noreply.github.com>
2025-02-09 12:03:16 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request # type: ignore
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import PIL
+import numpy as np	
+print(PIL.__version__)
+
+app = Flask(__name__)
+
+dic = {0 : 'Rp.1000',  
+       1 : 'Rp.10000',
+       2 : 'Rp.100000', 
+       3 : 'Rp.2000',
+	   4 : 'Rp.20000',
+	   5 : 'Rp.5000',
+	   6 : 'Rp.50000'
+	   }
+
+model = load_model('Rupiah-detect Exception-97.14.h5')
+
+model.make_predict_function()
+
+def predict_label(img_path):
+	i = image.load_img(img_path, target_size=(224,224))
+	i = image.img_to_array(i)/255.0
+	i = i.reshape(1, 224,224,3)
+
+	pred = model.predict(i)  # Menggunakan predict()
+	p = np.argmax(pred, axis=1)[0]  # Ambil indeks kelas dengan nilai tertinggi
+	return dic[p]
+
+
+# routes
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	return render_template("classification.html")
+
+
+@app.route("/submit", methods = ['GET', 'POST'])
+def get_output():
+	if request.method == 'POST':
+		img = request.files['my_image']
+
+		img_path = "static/" + img.filename	
+		img.save(img_path)
+
+		p = predict_label(img_path)
+
+	return render_template("classification.html", prediction = p, img_path = img_path)
+
+if __name__ =='__main__':
+	#app.debug = True
+	app.run(debug = True)
